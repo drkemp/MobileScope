ccfbe4dc013545c7d71dcabf847c078ffe8b9047
Mark Koudritsky <kamrik@gmail.com>
2014-03-06 17:24:40 -0500

    Polling for data every 50ms.

--- test/plot.js
+++ test/plot.js
@@ -1,92 +1,105 @@
 /* jshint jquery: true, browser: true */
-var plot, datasource;
+/* global signalPlugin*/
+
+var plot, datasource, trange;
 var i = 0;
+var theTimer;
+var isPlaying = false;
 
 
-function getRandomData() {
-    return [[1, 1], [2, 4], [3.5, 2.5], [4, 1 + i]];
+window.onload = onLoad;
+function onLoad() {
+    datasource = new signalPlugin('signals');
+    datasource.init();
+    datasource.setControls({mode: 'Stream'});
+    //plot = $.plot("#placeholder", [[0,0], [1,0]], settings);
+    redrawPlot();
+    //drawcontrols();
+
 }
 
-datasource = new signalPlugin('signals');
-
-var updateInterval = 30;
+// plot settings for flot lib
 var settings = {
     series: {
         shadowSize: 0 // Drawing is faster without shadows
     },
-    yaxis: {
-        min: 0,
-        max: 400
-    },
-    xaxis: {
-        min: 0,
-        show: true
-    }
+    yaxis: {},
+    xaxis: {}
 };
 
-
+// Update only the data without redrawing axes.
 function updatePlot() {
-
     var dt = Number(datasource.deltaT);
-    timestamps = _.range(0, dt * datasource.data.length, dt);
-    var plotData = _.zip(timestamps, datasource.data);
+    var plotData = prepData(trange);
     plot.setData([plotData]);
     // Since the axes don't change, we don't need to call plot.setupGrid()
     plot.draw();
 }
 
+// Get new data and massage it properly to be fed to flot.plot.
+function prepData(trange) {
+    var buffer = datasource.getData();
+    var t,v;
+    var dt = Number(datasource.deltaT);
+    var len = Math.ceil(trange / dt);
+    var dlen = buffer.data.length;
+    if (len > dlen) {
+        len = dlen;
+    }
+
+    var plotData = [];
+
+    for (var i=0; i<len; i++) {
+        t = -dt*i;
+        v = buffer.data[dlen-1-i];
+        //t = buffer.time[dlen-1-i];
+        //plotData.push([t, v]);
+        plotData.push([t, v]);
+    }
+    return plotData;
+}
+
+
+// Redraw the entire plot with axes and cursors
 function redrawPlot() {
+    var lines;
     trange = Number($('#trange').val()) / 1000;
-    ymin = Number($('#ymin').val());
-    ymax = Number($('#ymax').val());
-    console.log(ymax);
+    var ymin = Number($('#ymin').val());
+    var ymax = Number($('#ymax').val());
+
     settings.yaxis.min = ymin;
     settings.yaxis.max = ymax;
     settings.xaxis.min = -trange;
     settings.xaxis.max = 0;
 
-    var t,v;
-    var dt = Number(datasource.deltaT);
-    var len = Math.ceil(trange / dt);
-    var dlen = datasource.data.length;
-    if (len > dlen) {
-        len = dlen;
-    }
+    var plotData = prepData(trange);
 
-    plotData = [];
-
-    for (var i=0; i<len; i++) {
-        t = -dt*i;
-        v =  datasource.data[dlen-1-i];
-        plotData.push([t, v]);
-    }
-
-    t1 = Number($('#tcursor1').val());
-    t2 = Number($('#tcursor2').val());
-    tcursor1 = [[t1, ymin],[t1, ymax]];
-    tcursor2 = [[t2, ymin],[t2, ymax]];
+    var t1 = Number($('#tcursor1').val());
+    var t2 = Number($('#tcursor2').val());
+    var tcursor1 = [[t1, ymin],[t1, ymax]];
+    var tcursor2 = [[t2, ymin],[t2, ymax]];
     $('#tcursor_delta').text(t2-t1);
     if ($('#show_cursors').is(':checked')) {
-        plotData = [
+        lines = [
             plotData,
             tcursor1,
             tcursor2
         ];
+    } else {
+        lines = [ plotData ];
     }
-    plot = $.plot("#placeholder", plotData, settings);
-
-    // Since the axes don't change, we don't need to call plot.setupGrid()
-
-    plot.draw();
-    //setTimeout(update, updateInterval);
-    i = (i + 1) % 4;
+    plot = $.plot("#placeholder", lines, settings);
 }
 
+function playPause() {
+    if(isPlaying) {
+        clearInterval(theTimer);
+        isPlaying = false;
+        // Redraw the entire plot to show cursors.
+        redrawPlot();
+    } else {
+        theTimer = setInterval(updatePlot, 50);
+        isPlaying = true;
+    }
+}
 
-// On load
-$(function () {
-    datasource = new signalPlugin('signals');
-    datasource.init();
-    plot = $.plot("#placeholder", [[0,0], [1,0]], settings);
-    updatePlot();
-});
