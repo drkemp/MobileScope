6cb849b0d51c18db66e9852dc244982d9576e562
Mark Koudritsky <kamrik@gmail.com>
2014-03-10 02:22:26 -0400

    Add andoid theme.

--- test/background.js
+++ test/background.js
@@ -1,5 +1,5 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('main.html', {
+  chrome.app.window.create('plot.html', {
     'bounds': {
       'width': 600,
       'height': 400
