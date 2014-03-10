e2877ab92190efcb074eb07cace6af7525787af0
Mark Koudritsky <kamrik@gmail.com>
2014-03-10 02:23:23 -0400

    Add Angular based implementation of basic UI.

---
+++ ngapp/app/js/controllers.js
@@ -0,0 +1,119 @@
+'use strict';
+
+/* Controllers */
+
+/* jshint jquery: true, browser: true, globalstrict: true */
+/* global signalPlugin, angular */
+
+
+
+var mscopeApp = angular.module('mscopeApp', []);
+
+mscopeApp.controller('MScopeCtrl', function ($scope) {
+    var theTimer;
+
+    $scope.isPlaying = false;
+    $scope.ymin =0;
+    $scope.ymax = 300;
+    $scope.trange = 1.0;
+    $scope.show_cursors = true;
+    $scope.tcursor1 = -0.4;
+    $scope.tcursor2 = -0.45;
+
+
+
+    var src = $scope.datasource = new signalPlugin('signals');
+    src.init();
+    src.setControls({
+        mode: 'Stream'
+    });
+
+    // plot settings for flot lib
+    $scope.settings = {
+        series: {
+            shadowSize: 0 // Drawing is faster without shadows
+        },
+        yaxis: {},
+        xaxis: {}
+    };
+
+    redrawPlot();
+
+    // Update only the data without redrawing axes.
+    function updatePlot() {
+        var dt = Number($scope.datasource.deltaT);
+        var plot = $scope.plot;
+        var plotData = prepData($scope.trange);
+        plot.setData([plotData]);
+        // Since the axes don't change, we don't need to call plot.setupGrid()
+        plot.draw();
+    }
+
+    // Get new data and massage it properly to be fed to flot.plot.
+    function prepData(trange) {
+        var buffer = $scope.datasource.getData();
+        var t, v;
+        var dt = Number($scope.datasource.deltaT);
+        var len = Math.ceil(trange / dt);
+        var dlen = buffer.data.length;
+        if (len > dlen) {
+            len = dlen;
+        }
+
+        var plotData = [];
+
+        for (var i = 0; i < len; i++) {
+            t = -dt * i;
+            v = buffer.data[dlen - 1 - i];
+            //t = buffer.time[dlen-1-i];
+            //plotData.push([t, v]);
+            plotData.push([t, v]);
+        }
+        return plotData;
+    }
+
+
+    // Redraw the entire plot with axes and cursors
+    $scope.redrawPlot = redrawPlot;
+    function redrawPlot() {
+        var lines;
+
+        var settings = $scope.settings;
+        settings.yaxis.min = $scope.ymin;
+        settings.yaxis.max = $scope.ymax;
+        settings.xaxis.min = -$scope.trange;
+        settings.xaxis.max = 0;
+
+        var plotData = prepData($scope.trange);
+
+
+        var tcursor1 = [[$scope.tcursor1, $scope.ymin], [$scope.tcursor1, $scope.ymax]];
+        var tcursor2 = [[$scope.tcursor2, $scope.ymin], [$scope.tcursor2, $scope.ymax]];
+
+        if ($scope.show_cursors) {
+            lines = [
+            plotData,
+            tcursor1,
+            tcursor2
+        ];
+        } else {
+            lines = [plotData];
+        }
+        $scope.plot = $.plot("#graph", lines, settings);
+    }
+
+    $scope.playPause = playPause;
+    function playPause() {
+        if ($scope.isPlaying) {
+            clearInterval(theTimer);
+            $scope.isPlaying = false;
+            // Redraw the entire plot to show cursors.
+            redrawPlot();
+        } else {
+            theTimer = setInterval(updatePlot, 50);
+            $scope.isPlaying = true;
+        }
+    }
+
+
+});
